55c7c6f3f90a7bf4e327bd540e2919b7f8b8e24c
EdgarDjj <472484327@qq.com>
2020-10-12 16:58:17 +0800

    Initial commit

---
+++ src/main/java/com/ttt/chacha/chacha/ChachaApplication.java
@@ -0,0 +1,13 @@
+package com.ttt.chacha.chacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChachaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChachaApplication.class, args);
+    }
+
+}
